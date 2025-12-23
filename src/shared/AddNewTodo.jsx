11582c1e149dc8bc53b43bba98ab2334699e8eb7
akimanagbriel <gabsonbill@yahoo.com>
2025-12-23 18:35:54 +0200

    refactor changes

--- src/shared/AddNewTodo.jsx
+++ src/shared/AddNewTodo.jsx
@@ -1,5 +1,5 @@
 import { Check, X } from "lucide-react";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTodo } from "../context/Todo";
 
